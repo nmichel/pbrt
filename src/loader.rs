803db0ac582e175b6d7bcfdaaae3f8cea96a8621
Nicolas Michel <nicolas.michel.lava@gmail.com>
2026-08-21 14:56:36 +0200

    Hardcode temporarly a light source in the loader.

--- src/loader.rs
+++ src/loader.rs
@@ -15,20 +15,33 @@ use crate::colors;
 use crate::config::Config;
 use crate::geom::transform::Transform;
 use crate::geom::vector3::Vector3;
-use crate::lights::PointLight;
+use crate::lights::{BackgroundInfiniteLight, PointLight, UniformInfiniteLight};
+use crate::materials::{Lambertian, Metal};
+use crate::objects::Simple;
 use crate::scene::Scene;
+use crate::shapes::{Triangle, TriangleMesh};
+use crate::spectrum::Spectrum;
+use crate::textures::{CheckerBoard, PlainColor};
 
 pub struct Loader {}
 
 impl Loader {
     pub fn load_scene(input: &str, config: &Config) -> (Scene, Box<dyn Camera>) {
         let scene = Parser::parse(input);
+
         let mut visitor = SceneBuilderVisitor::new(config);
         visitor.visit(&scene);
         visitor.scene.commit();
 
-        // let point_light = PointLight::new(Box::new(Transform::translation(Vector3::new(0.0, 2.0, 0.0))), colors::WHITE * 1.0);
-        // visitor.scene.add_light(Arc::new(point_light));
+        visitor.scene.add_light(Arc::new(PointLight::new(
+            Box::new(Transform::translation(Vector3::new(0.0, 2.0, 1.0))),
+            colors::WHITE * 15.0,
+        )));
+        visitor
+            .scene
+            .add_light(Arc::new(BackgroundInfiniteLight::new(colors::WHITE, Spectrum::new(0.5, 0.7, 1.0))));
+        // visitor.scene.add_light(Arc::new(UniformInfiniteLight::new(colors::WHITE * 0.5)));
+
         (visitor.scene, visitor.camera.unwrap())
     }
 }
